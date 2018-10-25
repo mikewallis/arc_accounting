afd60727735d1bd145d267fe5a97c71aa6dc2f02
Mark Dixon <m.c.dixon@leeds.ac.uk>
2018-10-25 14:09:05 +0100

    Initial work on processing job syslog dataset

--- arc_accounting_python/feed_accounting.py
+++ arc_accounting_python/feed_accounting.py
@@ -13,6 +13,7 @@ import mysql.connector as mariadb
 import syslog
 import time
 import yaml
+import re
 
 
 # Initialise data
@@ -71,6 +72,7 @@ def main():
    parser = argparse.ArgumentParser(description='Feed accounting data')
    parser.add_argument('--service', action='store', type=str, help="Service name to tag records")
    parser.add_argument('--accountingfile', action='store', type=str, help="Accounting file to read from")
+   parser.add_argument('--syslogfile', action='store', type=str, help="Syslog file to read from")
    parser.add_argument('--sleep', action='store', type=int, default=300, help="Time to sleep between loop trips")
    parser.add_argument('--credfile', action='store', type=str, help="YAML credential file")
    args = parser.parse_args()
@@ -90,6 +92,13 @@ def main():
       ", ".join(['%(' + f + ')s' for f in fields]) + \
       ")"
 
+   syslog_select_record = "SELECT * FROM syslog_data WHERE cluster = %(cluster)s AND job = %(job)s"
+   syslog_add_record = "INSERT INTO syslog_data (cluster, job) VALUES (%(cluster)s, %(job)s)"
+   syslog_update_mpirun = "UPDATE syslog_data SET mpirun_file=%(mpirun_file)s WHERE cluster = %(cluster)s AND job = %(job)s"
+   syslog_update_sgealloc = "UPDATE syslog_data SET alloc=%(alloc)s WHERE cluster = %(cluster)s AND job = %(job)s"
+   syslog_update_sgenodes = "UPDATE syslog_data SET nodes_nodes=%(nodes_nodes)s, nodes_np=%(nodes_np)s, nodes_ppn=%(nodes_ppn)s, nodes_tpp=%(nodes_tpp)s WHERE cluster = %(cluster)s AND job = %(job)s"
+   syslog_update_modules = "UPDATE syslog_data SET modules=%(modules)s WHERE cluster = %(cluster)s AND job = %(job)s"
+
    syslog.openlog()
 
    # Try connecting to database and processing records.
@@ -98,7 +107,7 @@ def main():
       try:
          # Connect to database
          db = mariadb.connect(**credentials)
-         cursor = db.cursor()
+         cursor = db.cursor(dictionary=True)
 
          # Initialise state
 
@@ -115,6 +124,9 @@ def main():
             # Open input files and initialise state
             fh = open(args.accountingfile)
             record_num = 0
+
+         if args.syslogfile:
+            s_fh = open(args.syslogfile)
 
          # Process records as they come in
          while True:
@@ -139,15 +151,185 @@ def main():
                # - Commit bunch
                if 'start' in insert:
                   syslog.syslog("Inserting new " + args.service + \
-                         " records " + str(insert['start']) + " to " + \
-                         str(insert['end']))
+                         " sge accounting records " + str(insert['start']) + \
+                         " to " + str(insert['end']))
                   db.commit()
 
+
+            # Syslog records
+            if args.syslogfile:
+               insert = {}
+
+               # - Process any waiting lines
+               for record in syslog_records(file=s_fh):
+
+                  # Retrieve existing record
+                  cursor.execute(syslog_select_record, record)
+                  sql = cursor.fetchall()
+
+                  # Create/retrieve if does not exist
+                  if len(sql) < 1:
+                     cursor.execute(syslog_add_record, record)
+                     cursor.execute(syslog_select_record, record)
+                     sql = cursor.fetchall()
+
+                  # Update fields according to syslog data
+                  if record['type'] == "mpirun":
+                     # Add new mpirun file, comma separated (squash duplicates)
+                     if sql[0].get('mpirun_file', None):
+                        record['mpirun_file'] = ",".join(sorted(set([*sql[0]['mpirun_file'].split(','), record['mpirun_file']])))
+
+                     # Update record (if we've changed it)
+                     if sql[0]['mpirun_file'] != record['mpirun_file']:
+#                        print("update mpirun file", record['job'], record['mpirun_file'])
+                        cursor.execute(syslog_update_mpirun, record)
+#                     else:
+#                        print("SKIP mpirun")
+
+                  elif record['type'] == "sgealloc":
+                     if record['alloc']:
+                        if sql[0]['alloc'] != record['alloc']:
+#                           print("update sgealloc", sql, record)
+                           cursor.execute(syslog_update_sgealloc, record)
+#                        else:
+#                           print("SKIP sgealloc")
+
+                  elif record['type'] == "sgenodes":
+                     if record['nodes_nodes']:
+                        if sql[0]['nodes_nodes'] != int(record['nodes_nodes']) or \
+                           sql[0]['nodes_np'] != int(record['nodes_np']) or \
+                           sql[0]['nodes_ppn'] != int(record['nodes_ppn']) or \
+                           sql[0]['nodes_tpp'] != int(record['nodes_tpp']):
+
+#                           print("update sgenodes",sql,record)
+                           cursor.execute(syslog_update_sgenodes, record)
+
+                  elif record['type'] == "sgemodules" or \
+                       record['type'] == "module load":
+#                     print("module load", record)
+
+                     if record['modules']:
+                        m = record['modules'].split(':')
+
+                        if sql[0]['modules']:
+                           m.extend(sql[0]['modules'].split(','))
+
+                        record['modules'] = ','.join(sorted(set(m)))
+
+                        if sql[0]['modules'] != record['modules']:
+#                           print("update modules", record)
+                           cursor.execute(syslog_update_modules, record)
+
+                  else:
+                     print("What the?", record['type'])
+
+#                  print("here")
+
+                  db.commit()
+
+#               # - Commit bunch
+#               if 'start' in insert:
+#                  syslog.syslog("Inserting new " + args.service + \
+#                         " syslog records " + str(insert['start']) + \
+#                         " to " + str(insert['end']))
+#                  db.commit()
+
+            print("sleeping...")
             time.sleep(args.sleep)
       except:
          syslog.syslog("Processing failed" + str(sys.exc_info()))
 
       time.sleep(args.sleep)
 
-# Run the main program
-main()
+# Extract common features from syslog record
+# Jul 16 14:35:52 login1 someone: <data>
+syslog_def = re.compile(r"""
+   (?P<month>\S+)\s+
+   (?P<day>\d+)\s+
+   (?P<time>[0-9:]+)\s+
+   (?P<host>\S+)\s+
+   (?P<user>\S+):\s+
+   (?P<data>.*)
+""", re.VERBOSE)
+
+# mpirun data:
+# mpirun cluster=arc3_prod job= file=/home/home01/someone/misc/prog/fortran/hellompi
+mpirun_def = re.compile(r"""
+   (?P<type>mpirun)\s+
+   cluster=(?P<cluster>\S*)\s+
+   job=(?P<job>\S*)\s+
+   file=(?P<mpirun_file>\S*)
+""", re.VERBOSE)
+
+# sgealloc data:
+# sgealloc cluster=arc3_prod job=2555412.1 24core-128G.q@dc1s0b1b=1,24core-128G.q@dc1s0b1d=1
+sgealloc_def = re.compile(r"""
+   (?P<type>sgealloc)\s+
+   cluster=(?P<cluster>\S*)\s+
+   job=(?P<job>\S*)\s+
+   (?P<alloc>\S*)
+""", re.VERBOSE)
+
+# sgenodes data:
+# sgenodes cluster=arc3_prod job=1222658.1 nodes=12 np=288 ppn=24 tpp=1
+sgenodes_def = re.compile(r"""
+   (?P<type>sgenodes)\s+
+   cluster=(?P<cluster>\S*)\s+
+   job=(?P<job>\S*)\s+
+   nodes=(?P<nodes_nodes>\d*)\s+
+   np=(?P<nodes_np>\d*)\s+
+   ppn=(?P<nodes_ppn>\d*)\s+
+   tpp=(?P<nodes_tpp>\d*)
+""", re.VERBOSE)
+
+# sgemodules data:
+# sgemodules cluster= job=3609474.1624 licenses:sge:intel/17.0.1:openmpi/2.0.2:user
+sgemodules_def = re.compile(r"""
+   (?P<type>sgemodules)\s+
+   cluster=(?P<cluster>\S*)\s+
+   job=(?P<job>\S*)\s+
+   (?P<modules>.*)
+""", re.VERBOSE)
+
+# module load data:
+# job= module load sge intel/17.0.1 user openmpi/2.0.2 licenses (licenses:sge:intel/17.0.1:openmpi/2.0.2:user)
+
+##DEBUG - entries don't have a cluster attribute
+#   cluster=(?P<cluster>\S*)\s+
+
+sgemoduleload_def = re.compile(r"""
+   job=(?P<job>\S*)\s+
+   (?P<type>module\s+load)\s+
+   (?P<newmodules>.*)\s+
+   \((?P<modules>[^)]*)\)
+""", re.VERBOSE)
+
+# Return syslog records we are interested in
+def syslog_records(file):
+   for line in file:
+      r = syslog_def.match(line)
+      if r:
+         d = r.groupdict()
+
+         # Process different types of record:
+
+         for r_def in [ mpirun_def, sgemoduleload_def, sgealloc_def, sgenodes_def, sgemodules_def, sgemoduleload_def ]:
+            r_match = r_def.match(r['data'])
+            if r_match:
+               d_match = r_match.groupdict()
+
+               ##DEBUG - some records don't have a cluster yet
+               d_match['cluster'] = 'arc3_prod'
+
+               if d_match.get('cluster', False) and \
+                  d_match.get('job', False):
+
+                  yield({ **d, **d_match })
+                  break
+
+
+# Run program (if we've not been imported)
+# ---------------------------------------
+
+if __name__ == "__main__":
+   main()
