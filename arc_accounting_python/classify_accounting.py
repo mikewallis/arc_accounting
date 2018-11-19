eff211fbdf806bd01bfd87b96260bda18d9c15d0
Mark Dixon <m.c.dixon@leeds.ac.uk>
2018-11-19 11:29:43 +0000

    Initial attempt at the job classification step

---
+++ arc_accounting_python/classify_accounting.py
@@ -0,0 +1,255 @@
+#!/bin/env python
+
+# Classify database accounting records
+# pip install --user mysql-connector
+
+# Try and be python2 compatible
+from __future__ import print_function
+
+import argparse
+import sys
+import mysql.connector as mariadb
+import syslog
+import time
+import yaml
+import re
+
+
+def main():
+   # Command line arguments
+   parser = argparse.ArgumentParser(description='Classify accounting data')
+   parser.add_argument('--service', action='store', type=str, help="Service name to tag records")
+   parser.add_argument('--sleep', action='store', type=int, default=300, help="Time to sleep between loop trips")
+   parser.add_argument('--credfile', action='store', type=str, help="YAML credential file")
+   parser.add_argument('--debug', action='store_true', default=False, help="Print debugging messages")
+   args = parser.parse_args()
+
+   if not args.service:
+      raise SystemExit("Error: provide a service name argument")
+
+   if args.credfile:
+      with open(args.credfile, 'r') as stream:
+         credentials = yaml.safe_load(stream)
+   else:
+      raise SystemExit("Error: provide a database credential file")
+
+   syslog.openlog()
+
+   # Try connecting to database and processing records.
+   # Retry after a delay if there's a failure.
+   while True:
+      if args.debug: print("Entering main loop")
+      try:
+         # Connect to database
+         db = mariadb.connect(**credentials)
+
+         # Buffer results, as doing otherwise blocks other
+         # cursors from doing anything
+         cursor = db.cursor(dictionary=True, buffered=True)
+
+         while True:
+            # Do in batches, as buffered cursors means entire set is
+            # transferred at once.
+            cursor.execute("SELECT * FROM job_data WHERE classified=FALSE LIMIT 10000")
+            for sql in cursor:
+               classify(db, sql)
+
+            if args.debug: print("sleeping...")
+            time.sleep(args.sleep)
+      except:
+         syslog.syslog("Processing failed" + str(sys.exc_info()))
+
+      time.sleep(args.sleep)
+
+
+mpirun_match = [
+   { 'regex': '/vasp[/_0-9]', 'match': 'vasp' },
+   { 'regex': '/relion[/_0-9]', 'match': 'relion' },
+   { 'regex': '/lammps[/_0-9]', 'match': 'lammps' },
+   { 'regex': '/(wrf|wrfmeteo|geogrid|metgrid).exe$', 'match': 'wrf' },
+   { 'regex': '(^|/)amrvac$', 'match': 'amrvac' },
+   { 'regex': '(^|/)cesm.exe$', 'match': 'cesm' },
+   { 'regex': '(^|/)nek5000$', 'match': 'nek5000' },
+   { 'regex': 'Had(ley|CM3L)[^/]*.exec', 'match': 'um' },
+   { 'regex': '/castep(/|.mpi$|$)', 'match': 'castep' },
+   { 'regex': '/OpenFOAM/', 'match': 'openfoam' },
+   { 'regex': '/BISICLES/', 'match': 'bisicles' },
+   { 'regex': '/gulp(.mpi)?$/', 'match': 'gulp' },
+]
+
+application_modules = [
+   'abaqus',
+   'amber',
+   'ampl',
+   'ansys',
+   'ansysem',
+   'cfdem',
+   'comsol',
+   'cp2k',
+   'crystal17',
+   'delft3d',
+   'dl_poly',
+   'dosbox',
+   'fcm',
+   'feff',
+   'gaussian',
+   'gmt',
+   'gromacs',
+   'h5utils',
+   'idl',
+   'lammps',
+   'liggghts',
+   'matlab',
+   'meep',
+   'mesmer',
+   'molpro',
+   'mpb',
+   'namd',
+   'nco',
+   'openeye',
+   'openfoam',
+   'orca',
+   'paraview',
+   'paraview-osmesa',
+   'schrodinger',
+   'starccm',
+   'stata',
+   'tetr',
+   'visit',
+   'vmd',
+
+   'singularity',
+   'R',
+   'python',
+]
+
+def classify(db, record):
+
+   cursor = db.cursor()
+
+   # Init classifications
+   application = None
+   appsource = None
+   parallel = None
+
+
+   # Check mpirun data
+
+   if not application:
+      cursor.execute(
+         """
+            SELECT
+               mpirun.name
+            FROM
+               job_to_mpirun, mpirun
+            WHERE
+               job_to_mpirun.mpirunid = mpirun.id
+            AND
+               job_to_mpirun.jobid = %s
+         """,
+         (record['id'], ),
+      )
+
+      for rec in cursor:
+         file = rec[0]
+
+         # Check if it's one of our applications
+         if not application:
+            r = re.search('^/apps/[^/]+/([^/]+)/([^/]+)/', file)
+            if r:
+               application = r.group(0)
+               appsource = 'module'
+               parallel = 'mpi'
+
+         # Check if it's obvious from filename
+         if not application:
+            for m in mpirun_match:
+               if re.search(m['regex'], file):
+                  application = m['match']
+                  appsource = 'user'
+                  parallel = 'mpi'
+                  break
+
+
+   # Check module data
+
+   if not application:
+      cursor.execute(
+         """
+            SELECT
+               module.name
+            FROM
+               job_to_module, module
+            WHERE
+               job_to_module.moduleid = module.id
+            AND
+               job_to_module.jobid = %s
+         """,
+         (record['id'], ),
+      )
+
+      for rec in cursor:
+         module = rec[0]
+
+         if not application:
+            for app in application_modules:
+               if re.search("^"+ app +"/", module):
+                  application = app
+                  appsource = 'module'
+                  break
+
+
+   # Check job details
+
+   if not parallel:
+      cursor.execute(
+         """
+            SELECT
+               slots
+            FROM
+               accounting_sge
+            WHERE
+               service = %(service)s
+            AND
+               job = %(job)s
+         """,
+         record,
+      )
+
+      for acct in cursor:
+         slots = acct[0]
+         if slots == 1:
+            parallel = 'serial'
+         else:
+            parallel = 'threaded'
+
+
+   # Save results, mark as classified
+
+   cursor.execute(
+      """
+         UPDATE
+            job_data
+         SET
+            classified=TRUE,
+            class_app = %s,
+            class_appsource = %s,
+            class_parallel = %s
+         WHERE
+            id = %s
+      """,
+      (application, appsource, parallel, record['id'], ),
+   )
+
+
+   #if args.debug: print(record['id'], "class_app", application)
+   print(record['id'], application, appsource, parallel)
+
+   db.commit()
+
+
+# Run program (if we've not been imported)
+# ---------------------------------------
+
+if __name__ == "__main__":
+   main()
